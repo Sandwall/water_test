f45d206a3c9a1cf8d9cb42c50b50703b04f2772c
Sandwall <17184361+Sandwall@users.noreply.github.com>
2026-01-17 15:14:25 -0500

    added skeleton of ewave class

---
+++ src/ewave.cpp
@@ -0,0 +1,40 @@
+#include "ewave.h"
+
+#include <stdlib.h> // for calloc/free
+#include <math.h>
+#include <algorithm>
+
+#include <GL/gl3w.h>
+#include "gl_renderer.h"
+
+// NOTE: i'm lazy lol
+#define DOALLOC static_cast<float*>(malloc(bufferSize))
+#define DOFREE(x) free(x); x = nullptr;
+#define SETZERO(x) memset(x, 0, bufferSize)
+
+EWaveSurface::EWaveSurface(int w, int h) {
+
+}
+
+EWaveSurface::~EWaveSurface() {
+
+}
+void EWaveSurface::place_source(int x, int y, float r, float strength) {
+
+}
+
+void EWaveSurface::set_obstruction(int x, int y, float r, float strength) {
+
+}
+
+void EWaveSurface::sim_frame(float delta) {
+
+}
+
+void EWaveSurface::reset() {
+
+}
+
+GLuint EWaveSurface::get_display() {
+
+}
--- src/ewave.h
+++ src/ewave.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include "surface_sim.h"
+
 // TODO: I'll get to this eventually
 // https://people.computing.clemson.edu/~jtessen/students/goswami_thesis.pdf
-class EWaveSurface {
+class EWaveSurface : SurfaceSim {
 	int width, height;
 public:
 	EWaveSurface(int w, int h);
 	~EWaveSurface();
+
+	void place_source(int x, int y, float r, float strength) override;
+	void set_obstruction(int x, int y, float r, float strength) override;
+
+	void sim_frame(float delta) override;
+
+	void reset() override;
+	GLuint get_display() override;
 };
